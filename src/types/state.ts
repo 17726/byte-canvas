3ded1a3ad7f47548ef9988830821967235cbf2bf
Treasure-cool <2899465094@qq.com>
2025-11-25 16:26:45 +0800

    feat:add  clientToWorld and  InternalDragState

--- src/types/state.ts
+++ src/types/state.ts
@@ -101,5 +101,13 @@ export interface GroupState extends BaseNodeState {
   children: string[];
 }
 
+export interface InternalDragState {
+  isDragging: boolean;//是否正在拖拽
+  type: 'move' | null;//拖拽类型：移动/缩放/旋转
+  nodeId: string;//被拖节点的唯一标识
+  startMouseX: number;//拖拽起始时鼠标坐标
+  startMouseY: number;
+  startTransform: TransformState; // 拖拽起始的变换数据
+} 
 /** 5. 所有 State 类型的联合 */
 export type NodeState = ShapeState | TextState | ImageState | GroupState;
